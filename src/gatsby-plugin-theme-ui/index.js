69a5d06c988b7e042cc77e33e7c33aedea6b0ec0
Manh Hung <hungdm1211@gmail.com>
2020-05-02 17:08:17 +0700

    add pre tag colors

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -20,11 +20,25 @@ export default {
     text: '#000000',
     background: '#ffffff',
     primary: '#4433FF',
-    secondary: '#51EAEA',
+    secondary: '#e60067',
+    tertiary: '#2c0b8e',
     muted: '#E2E8F0',
     homepageLight: '#D3EAF8',
     homepageDark: '#BFDEF2',
     bluredBackground: 'rgba(255, 255, 255, 0.85)',
+    preBackground: '#f5f6f9',
+    preText: '#2A2A2A',
+    tokenComment: '#467790',
+    tokenPunctuation: '#999',
+    tokenFunction: '#3D5AFE',
+    tokenProp: '#da0079',
+    tokenName: '#AA00FF',
+    tokenStr: '#651fff',
+    tokenVal: '#78909C',
+    tokenRegex: '#e90',
+    tokenHightlight: '#e9ebf2',
+    tokenBool: '#bf00b8',
+    tokenDel: '#c8b8f9',
   },
   text: {
     heading: {
@@ -53,7 +67,7 @@ export default {
     },
     h1: {
       variant: 'text.heading',
-      fontSize: 5,
+      fontSize: '60px',
     },
     h2: {
       variant: 'text.heading',
@@ -80,6 +94,46 @@ export default {
       overflowX: 'auto',
       code: {
         color: 'inherit',
+      },
+      '.token.comment,.token.prolog,.token.doctype, .token.cdata': {
+        color: 'tokenComment',
+      },
+      '.token.punctuation': {
+        color: 'tokenPunctuation',
+      },
+      '.token.class-name, .token.tag, .token.constant, .token.parameter, .token.tag.punctiation, .token.template-string.interpolation.constant': {
+        color: 'tokenProp',
+      },
+      '.token.function': {
+        color: '#3D5AFE',
+      },
+      '.token.selector, .token.attr-name, .token.char, .token.builtin, .token.inserted, .token.script-punctuation': {
+        color: 'tokenName',
+      },
+      '.token.keyword, .token.operator, .token.entity, .token.dom.variable, .token.variable, .token.dom, .token.string, .token.script.string, .token.template-string.template-punctuation, .token.template-string, .token.tag.string, .token.tag.operator, .token.url,.language-css.token.string,.style.token.string': {
+        color: 'tokenStr',
+      },
+      '.token.attr-value, .token.atrule': {
+        color: 'tokenVal',
+      },
+      '.token.regex, .token.important': {
+        color: 'tokenRegex',
+      },
+      '.token.number, .token.boolean, .token.property, .token.symbol, .token.deleted': {
+        color: 'tokenBool',
+      },
+      '.token.namespace': {
+        opacity: '.7',
+      },
+      '.token.important, .token.bold': {
+        fontWeight: 'bold',
+      },
+      '.token.italic': {
+        fontStyle: 'italic',
+      },
+
+      '.token.entity': {
+        cursor: 'help',
       },
     },
     code: {
