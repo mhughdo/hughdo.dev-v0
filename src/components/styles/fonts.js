703a40037b565ee5ead8b2cc31f85d2645b3abe6
Manh Hung <hungdm1211@gmail.com>
2020-05-03 22:29:13 +0700

    much smaller fontsize

--- src/components/styles/fonts.js
+++ src/components/styles/fonts.js
@@ -2,11 +2,17 @@ import {css} from '@emotion/core'
 import Jost300Light from '../../assets/fonts/Jost/Jost-300-Light.woff2'
 import Jost300LightItalic from '../../assets/fonts/Jost/Jost-300-LightItalic.woff2'
 import Jost400Book from '../../assets/fonts/Jost/Jost-400-Book.woff2'
+import Jost400BookTTF from '../../assets/fonts/Jost/Jost-400-Book.ttf'
+import Jost400BookWOFF from '../../assets/fonts/Jost/Jost-400-Book.woff'
 import Jost400BookItalic from '../../assets/fonts/Jost/Jost-400-BookItalic.woff2'
 import Jost500Medium from '../../assets/fonts/Jost/Jost-500-Medium.woff2'
+import Jost500MediumTTF from '../../assets/fonts/Jost/Jost-500-Medium.ttf'
+import Jost500MediumWOFF from '../../assets/fonts/Jost/Jost-500-Medium.woff'
 import Jost500MediumItalic from '../../assets/fonts/Jost/Jost-500-MediumItalic.woff2'
 import Jost600Semi from '../../assets/fonts/Jost/Jost-600-Semi.woff2'
 import Jost700Bold from '../../assets/fonts/Jost/Jost-700-Bold.woff2'
+import Jost700BoldTTF from '../../assets/fonts/Jost/Jost-700-Bold.ttf'
+import Jost700BoldWOFF from '../../assets/fonts/Jost/Jost-700-Bold.woff'
 import PoppinsLight from '../../assets/fonts/Poppins/Poppins-Light.woff2'
 import PoppinsLightItalic from '../../assets/fonts/Poppins/Poppins-LightItalic.woff2'
 import PoppinsRegular from '../../assets/fonts/Poppins/Poppins-Regular.woff2'
@@ -33,7 +39,8 @@ export default css`
     font-family: Jost;
     font-weight: normal;
     font-style: normal;
-    src: url(${Jost400Book}) format('woff2');
+    src: url(${Jost400Book}) format('woff2'), url(${Jost400BookTTF}) format('woff'),
+      url(${Jost400BookWOFF}) format('truetype');
   }
   @font-face {
     font-family: Jost;
@@ -45,7 +52,8 @@ export default css`
     font-family: Jost;
     font-weight: 500;
     font-style: normal;
-    src: url(${Jost500Medium}) format('woff2');
+    src: url(${Jost500Medium}) format('woff2'), url(${Jost500MediumWOFF}) format('woff'),
+      url(${Jost500MediumTTF}) format('truetype');
   }
   @font-face {
     font-family: Jost;
@@ -64,7 +72,8 @@ export default css`
     font-family: Jost;
     font-weight: 700;
     font-style: normal;
-    src: url(${Jost700Bold}) format('woff2');
+    src: url(${Jost700Bold}) format('woff2'), url(${Jost700BoldWOFF}) format('woff'),
+      url(${Jost700BoldTTF}) format('truetype');
   }
 
   @font-face {
