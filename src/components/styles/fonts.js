127903d9a6900db28ec7e13d4a4a705fd73a2010
Manh Hung <hungdm1211@gmail.com>
2020-05-03 20:24:51 +0700

    optimize font loading

--- src/components/styles/fonts.js
+++ src/components/styles/fonts.js
@@ -1,102 +1,134 @@
 import {css} from '@emotion/core'
-import Jost300Light from '../../assets/fonts/Jost/Jost-300-Light.ttf'
-import Jost300LightItalic from '../../assets/fonts/Jost/Jost-300-LightItalic.ttf'
-import Jost400Book from '../../assets/fonts/Jost/Jost-400-Book.ttf'
-import Jost400BookItalic from '../../assets/fonts/Jost/Jost-400-BookItalic.ttf'
-import Jost500Medium from '../../assets/fonts/Jost/Jost-500-Medium.ttf'
-import Jost600Semi from '../../assets/fonts/Jost/Jost-600-Semi.ttf'
-import Jost700Bold from '../../assets/fonts/Jost/Jost-700-Bold.ttf'
-import PoppinsLight from '../../assets/fonts/Poppins/Poppins-Light.ttf'
-import PoppinsLightItalic from '../../assets/fonts/Poppins/Poppins-LightItalic.ttf'
-import PoppinsRegular from '../../assets/fonts/Poppins/Poppins-Regular.ttf'
-import PoppinsItalic from '../../assets/fonts/Poppins/Poppins-Italic.ttf'
-import PoppinsMedium from '../../assets/fonts/Poppins/Poppins-Medium.ttf'
-import PoppinsSemiBold from '../../assets/fonts/Poppins/Poppins-SemiBold.ttf'
-import PoppinsBold from '../../assets/fonts/Poppins/Poppins-Bold.ttf'
+import Jost300Light from '../../assets/fonts/Jost/Jost-300-Light.woff2'
+import Jost300LightItalic from '../../assets/fonts/Jost/Jost-300-LightItalic.woff2'
+import Jost400Book from '../../assets/fonts/Jost/Jost-400-Book.woff2'
+import Jost400BookItalic from '../../assets/fonts/Jost/Jost-400-BookItalic.woff2'
+import Jost500Medium from '../../assets/fonts/Jost/Jost-500-Medium.woff2'
+import Jost500MediumItalic from '../../assets/fonts/Jost/Jost-500-MediumItalic.woff2'
+import Jost600Semi from '../../assets/fonts/Jost/Jost-600-Semi.woff2'
+import Jost700Bold from '../../assets/fonts/Jost/Jost-700-Bold.woff2'
+import PoppinsLight from '../../assets/fonts/Poppins/Poppins-Light.woff2'
+import PoppinsLightItalic from '../../assets/fonts/Poppins/Poppins-LightItalic.woff2'
+import PoppinsRegular from '../../assets/fonts/Poppins/Poppins-Regular.woff2'
+import PoppinsItalic from '../../assets/fonts/Poppins/Poppins-Italic.woff2'
+import PoppinsMedium from '../../assets/fonts/Poppins/Poppins-Medium.woff2'
+import PoppinsMediumItalic from '../../assets/fonts/Poppins/Poppins-MediumItalic.woff2'
+import PoppinsSemiBold from '../../assets/fonts/Poppins/Poppins-SemiBold.woff2'
+import PoppinsBold from '../../assets/fonts/Poppins/Poppins-Bold.woff2'
 
 export default css`
   @font-face {
     font-family: Jost;
     font-weight: 300;
     font-style: normal;
-    src: url(${Jost300Light}) format('truetype');
+    src: url(${Jost300Light}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Jost;
     font-weight: 300;
     font-style: italic;
-    src: url(${Jost300LightItalic}) format('truetype');
+    src: url(${Jost300LightItalic}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Jost;
     font-weight: normal;
     font-style: normal;
-    src: url(${Jost400Book}) format('truetype');
+    src: url(${Jost400Book}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Jost;
     font-weight: normal;
     font-style: italic;
-    src: url(${Jost400BookItalic}) format('truetype');
+    src: url(${Jost400BookItalic}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Jost;
     font-weight: 500;
     font-style: normal;
-    src: url(${Jost500Medium}) format('truetype');
+    src: url(${Jost500Medium}) format('woff2');
+    font-display: 'swap';
   }
+  @font-face {
+    font-family: Jost;
+    font-weight: 500;
+    font-style: italic;
+    src: url(${Jost500MediumItalic}) format('woff2');
+    font-display: 'swap';
+  }
+
   @font-face {
     font-family: Jost;
     font-weight: 600;
     font-style: normal;
-    src: url(${Jost600Semi}) format('truetype');
+    src: url(${Jost600Semi}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Jost;
     font-weight: 700;
     font-style: normal;
-    src: url(${Jost700Bold}) format('truetype');
+    src: url(${Jost700Bold}) format('woff2');
+    font-display: 'swap';
   }
+
   @font-face {
     font-family: Poppins;
     font-weight: normal;
     font-style: normal;
-    src: url(${PoppinsRegular}) format('truetype');
+    src: url(${PoppinsRegular}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: 300;
     font-style: normal;
-    src: url(${PoppinsLight}) format('truetype');
+    src: url(${PoppinsLight}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: 300;
     font-style: italic;
-    src: url(${PoppinsLightItalic}) format('truetype');
+    src: url(${PoppinsLightItalic}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: normal;
     font-style: italic;
-    src: url(${PoppinsItalic}) format('truetype');
+    src: url(${PoppinsItalic}) format('woff2');
+    font-display: 'swap';
+  }
+  @font-face {
+    font-family: Poppins;
+    font-weight: 500;
+    font-style: normal;
+    src: url(${PoppinsMedium}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: 500;
     font-style: italic;
-    src: url(${PoppinsMedium}) format('truetype');
+    src: url(${PoppinsMediumItalic}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: 600;
     font-style: normal;
-    src: url(${PoppinsSemiBold}) format('truetype');
+    src: url(${PoppinsSemiBold}) format('woff2');
+    font-display: 'swap';
   }
   @font-face {
     font-family: Poppins;
     font-weight: 700;
     font-style: normal;
-    src: url(${PoppinsBold}) format('truetype');
+    src: url(${PoppinsBold}) format('woff2');
+    font-display: 'swap';
   }
 `
